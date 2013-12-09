3154f4ed7e701d8399947d9bcb4ffd28e65a227e
hubba-bubba <jacksincalgary+github@gmail.com>
2013-12-09 07:45:39 -0700

    Update xbmc.cpp
    Added a comment just to see if this file updates when re-installing overtop of an existing xbmc install.

--- xbmc/xbmc.cpp
+++ xbmc/xbmc.cpp
@@ -1,7 +1,7 @@
 /*
  *      Copyright (C) 2005-2013 Team XBMC
  *      http://xbmc.org
- *
+ *      Andrew says Hello!
  *  This Program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2, or (at your option)
